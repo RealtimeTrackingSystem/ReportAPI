099d888e83329d367c6412ce2f45a288d99e9fac
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-07-26 01:38:23 +0800

    added env variables

--- src/config/development.js
+++ src/config/development.js
@@ -4,7 +4,12 @@ const CONFIG = {
     env: 'dev'
   },
   SALT: 10,
-  SECRET_KEY: 'thesupersecretkey'
+  SECRET_KEY: 'thesupersecretkey',
+  SENDGRID: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD,
+    SENDGRID_USERNAME: process.env.SENDGRID_USERNAME
+  }
 };
 
 module.exports = CONFIG;
