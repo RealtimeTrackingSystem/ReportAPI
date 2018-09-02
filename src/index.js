f5a7a2f091729759e7bf32fdc0d5509bbb44d0d3
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-09-03 02:16:05 +0800

    update

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.use(morgan('combined'));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Api-Key');
   next();
 });
 
