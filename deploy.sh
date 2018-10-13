85ab3be641e7e99e4dbb46b576acf9ac63b55aff
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-10-13 22:38:45 +0800

    id: ISSUES-64 G-Cloud Deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 docker build -t johnhigginsavila/report-api:latest -t johnhigginsavila/report-api:$SHA -f ./Dockerfile .
 docker push johnhigginsavila/report-api:latest
 docker push johnhigginsavila/report-api:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/report-api-deployment report-api=johnhigginsavila/report-api:$SHA
