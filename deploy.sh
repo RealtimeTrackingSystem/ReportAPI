4cf6fdf2074ec55eefcef5816fe3bd7d1d78e2f1
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-12-09 21:27:27 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+tar xvf secrets.tar
 docker build -t johnhigginsavila/report-api:latest -t johnhigginsavila/report-api:$SHA -f ./Dockerfile .
 docker push johnhigginsavila/report-api:latest
 docker push johnhigginsavila/report-api:$SHA
