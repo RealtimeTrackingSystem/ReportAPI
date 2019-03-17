b14a9491331430e386c2499bf9f321d10020e54b
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2019-03-17 12:23:23 +0800

    redeployment

--- src/config/development.js
+++ src/config/development.js
@@ -1,8 +1,8 @@
 const CONFIG = {
   db: {
-    HOST: process.env.DB_HOST,
-    PORT: process.env.DB_PORT,
-    DATABASE: process.env.DATABASE,
+    HOST: process.env.DB_HOST || '127.0.0.1',
+    PORT: process.env.DB_PORT || '27017',
+    DATABASE: process.env.DATABASE || 'reportApiDb2',
     MONGO_INITDB_ROOT_USERNAME: process.env.MONGO_INITDB_ROOT_USERNAME,
     MONGO_INITDB_ROOT_PASSWORD: process.env.MONGO_INITDB_ROOT_PASSWORD
   },
@@ -16,9 +16,9 @@ const CONFIG = {
   SALT: 10,
   SECRET_KEY: 'thesupersecretkey',
   SENDGRID: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD,
-    SENDGRID_USERNAME: process.env.SENDGRID_USERNAME
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || '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',
+    SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD || 'zlvmvall6181',
+    SENDGRID_USERNAME: process.env.SENDGRID_USERNAME || 'app113302926@heroku.com'
   }
 };
 
