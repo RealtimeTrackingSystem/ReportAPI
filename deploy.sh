8888bf7a88da7f66d2de0521b5c4571f4f752997
John Higgins Avila <johnhiggins.avila@gmail.com>
2019-03-17 16:09:50 +0800

    Feature/mediation (#69)
    * redoployment
    
    * redeployment
    
    * fix-secret
    
    * fixing-creds
    
    * test
    
    * fix

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
+docker build -t johnhigginsavila/middleware-api:latest -t johnhigginsavila/middleware-api:$SHA -f ./Dockerfile .
+docker push johnhigginsavila/middleware-api:latest
+docker push johnhigginsavila/middleware-api:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/report-api-deployment report-api=johnhigginsavila/report-api:$SHA
