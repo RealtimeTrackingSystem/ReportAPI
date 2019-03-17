8fcc0ab0263eed21a554aea63bf4419193f31a3b
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2019-03-17 16:17:12 +0800

    fix-deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker build -t johnhigginsavila/middleware-api:latest -t johnhigginsavila/middleware-api:$SHA -f ./Dockerfile .
-docker push johnhigginsavila/middleware-api:latest
-docker push johnhigginsavila/middleware-api:$SHA
+docker build -t johnhigginsavila/report-api-api:latest -t johnhigginsavila/report-api:$SHA -f ./Dockerfile .
+docker push johnhigginsavila/report-api:latest
+docker push johnhigginsavila/report-api:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/report-api-deployment report-api=johnhigginsavila/report-api:$SHA
